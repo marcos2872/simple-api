56132c92ceebf1d980f306abf322863908179ef0
Marcos Brito <marcos.brito@sp.senai.br>
2025-10-24 15:47:15 -0300

    feat: Enable CORS and add logging for SSE connections in MCP controller

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
--- src/mcp/controllers/mcp.controlle.ts
+++ src/mcp/controllers/mcp.controlle.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res, Logger } from '@nestjs/common';
 import { McpSseService } from '@rekog/mcp-nest';
 
 @Controller()
 export class CustomSseController {
+  private readonly logger = new Logger(CustomSseController.name);
+
   constructor(private readonly mcpStreamableHttpService: McpSseService) {}
 
   @Get('sse')
   async connectionSse(@Req() req: any, @Res() res: any): Promise<void> {
+    this.logger.log('SSE connection attempt');
+
     await this.mcpStreamableHttpService.createSseConnection(
       req,
       res,
@@ -21,6 +25,7 @@ export class CustomSseController {
     @Res() res: any,
     @Body() body: any,
   ): Promise<void> {
+    this.logger.log('Handling SSE message');
     await this.mcpStreamableHttpService.handleMessage(req, res, {
       ...body,
       // params: { ...body.params, user: req.user },
